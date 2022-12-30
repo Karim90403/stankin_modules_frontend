0e29f71426a13b7c1d21e4676df8e1bced200f0d
Karim90403 <karim90403@gmail.com>
2022-12-31 02:15:00 +0500

    Доработаны модули и список групп

--- src/views/ModulesView.vue
+++ src/views/ModulesView.vue
@@ -17,13 +17,14 @@ const modules = ref<Array<any>>([
 
 <template>
   <div class="flex justify-center items-center h-screen">
-    <table class="w-4/5 shadow-2xl table-auto rounded bg-white sm:w-1/2">
-      <thead>
+    <table class="w-4/5 shadow-2xl table-auto    Бефиду сдфыы=Эц-4/5 ырфвщц-2чд ефиду-фгещ кщгтвув ип-цршеу ыьЖц-1/2ЭЮ
+ rounded bg-white sm:w-1/2">
+      <thead class="text-sm sm:text-base">
         <tr class="border-b-2 border-gray-500">
-          <td class="border-r border-gray-500">Предмет</td>
-          <td class="border-r border-gray-500 text-center">М1</td>
-          <td class="border-r border-gray-500 text-center">М2</td>
-          <td class="border-r border-gray-500 text-center">Зач</td>
+          <td>Предмет</td>
+          <td class=" text-center">М1</td>
+          <td class=" text-center">М2</td>
+          <td class=" text-center">Зач</td>
           <td>Экз</td>
         </tr>
       </thead>
@@ -32,7 +33,7 @@ const modules = ref<Array<any>>([
           :firstModule=subject.firstModule :secondModule=subject.secondModule :offset="subject.offset"
           :exam="subject.exam"></Module>
         <tr>
-          <td class="">
+          <td class="text-xs sm:text-base">
             Ожидаемый рейтинг: 54
           </td>
         </tr>
