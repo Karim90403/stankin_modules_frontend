3c872aacb2dc51e1998ea59483c153637340d6a1
Karim90403 <karim90403@gmail.com>
2023-02-08 01:53:43 +0300

    Added registration with the API module journal

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ const toModules = ():void => {
 
 const toTimetable = ():void => {
   if(!store.state.isTimetable){
-    router.push('/timetable')
+    router.push('/')
     store.state.isModules = false;
     store.state.isTimetable = true;
     store.state.isOtherPeople = false;
@@ -35,13 +35,12 @@ const toOtherPeople = ():void => {
 }
 
 onMounted(() => {
-  console.log(useStore())
     if(!store.state.verify){
       router.push('/register')
     }
     else{
       if(store.state.group.length>0){
-      router.push('/timetable')
+      router.push('/')
       }
       else{
         router.push('/otherPeople')
@@ -53,27 +52,33 @@ onMounted(() => {
 <template>
   <div v-if="store.state.verify" class="p-4 top-1/3 absolute hidden w-24 sm:block">
     <div class="py-4 px-2 text-gray-900 bg-white rounded shadow-lg ">
-      <span class="cursor-pointer block transition duration-300 rounded-full mb-5" @click="toModules()" :class="store.state.isModules ? 'text-emerald-300' : 'text-slate-400'">
+      <span class="cursor-pointer block transition text-slate-400 duration-300 rounded-full mb-5">
+        <BaseIcon icon="user" viewBox="0 0 16 16" class="fill-current"/>
+      </span>
+      <span class="cursor-pointer block transition duration-300 rounded-full mb-5" @click="toModules()" :class="store.state.isModules ? 'text-blue-300' : 'text-slate-400'">
         <BaseIcon icon="modules" viewBox="0 0 100 100" class="fill-current"/>
       </span>
-      <span class="cursor-pointer px-2 transition duration-300 block mb-5" @click="toTimetable()" :class="store.state.isTimetable ? 'text-emerald-300' : 'text-slate-400'">
+      <span class="cursor-pointer px-2 transition duration-300 block mb-5" @click="toTimetable()" :class="store.state.isTimetable ? 'text-blue-300' : 'text-slate-400'">
         <BaseIcon icon="calendar" viewBox="0 0 20 20" class="fill-current"/>
       </span>
-      <span class="cursor-pointer px-2 transition duration-300 block" @click="toOtherPeople()" :class="store.state.isOtherPeople ? 'text-emerald-300' : 'text-slate-400'">
+      <span class="cursor-pointer px-2 transition duration-300 block" @click="toOtherPeople()" :class="store.state.isOtherPeople ? 'text-blue-300' : 'text-slate-400'">
         <BaseIcon icon="others" viewBox="0 0 56 56" class="fill-current"/>
       </span>
     </div>
   </div>
 
   <div v-if="store.state.verify" class="p-4 w-full block fixed bottom-0 sm:hidden">
-      <div class="p-2 text-gray-900 bg-white rounded-lg shadow-lg flex justify-around">
-        <span class="cursor-pointer w-12 px-2 block transition duration-300" @click="toModules()" :class="store.state.isModules ? 'text-emerald-300' : 'text-slate-400'">
+      <div class="p-2 text-gray-900 bg-white rounded-lg shadow-lg drop-shadow-xl flex justify-around">
+        <span class="cursor-pointer w-12 px-2 block transition duration-300 text-slate-400">
+          <BaseIcon icon="user" viewBox="0 0 16 16" class="fill-current"/>
+        </span>
+        <span class="cursor-pointer w-12 px-2 block transition duration-300" @click="toModules()" :class="store.state.isModules ? 'text-blue-300' : 'text-slate-400'">
           <BaseIcon icon="modules" viewBox="0 0 100 100" class="fill-current"/>
         </span>
-        <span class="cursor-pointer w-12 px-2 block transition duration-300" @click="toTimetable()" :class="store.state.isTimetable ? 'text-emerald-300' : 'text-slate-400'">
+        <span class="cursor-pointer w-12 px-2 block transition duration-300" @click="toTimetable()" :class="store.state.isTimetable ? 'text-blue-300' : 'text-slate-400'">
           <BaseIcon icon="calendar" viewBox="0 0 20 20" class="fill-current"/>
         </span>
-        <span class="cursor-pointer w-12 px-2 block transition duration-300" @click="toOtherPeople()" :class="store.state.isOtherPeople ? 'text-emerald-300' : 'text-slate-400'">
+        <span class="cursor-pointer w-12 px-2 block transition duration-300" @click="toOtherPeople()" :class="store.state.isOtherPeople ? 'text-blue-300' : 'text-slate-400'">
           <BaseIcon icon="others" viewBox="0 0 56 56" class="fill-current"/>
         </span>
       </div>
